3ea9900a3133ea8c0941eebec9ca3abaa2ffbfd6
CarlyCallans <ccallans@lion.lmu.edu>
2015-03-02 16:04:09 -0800

    Update and rename Vector to Vector/Timer.java

---
+++ Vector/Timer.java
@@ -0,0 +1,28 @@
+public class Timer {
+    
+    private double currentTime;
+    private double grain;
+    
+    // Create a timer starting at 0 and using a 10 second grain/timeslice
+    public Timer() {
+        // Implement me!
+    }
+    
+    // Create a timer starting at 0 and using the given grain/timeslice in seconds
+    public Timer(double grain) {
+        // Implement me!
+    }
+    
+    public double getCurrentTime() {
+        // Implement me!
+    }
+    
+    public double getGrain() {
+        // Implement me!
+    }
+    
+    // Increment the timer by the grain/timeslice.
+    public void tick() {
+        // Implement me!
+    }
+}
