1dbb7e99d478f02ca3fff4b94119046ca74e7bb8
CarlyCallans <ccallans@lion.lmu.edu>
2015-03-02 16:03:27 -0800

    Create Vector.java

---
+++ Vector/Vector.java
@@ -0,0 +1,26 @@
+public class Vector {
+
+    // Declare my instance variables!
+    
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double x() {
+        return x;
+    }
+    
+    public double y() {
+        return y;
+    }
+
+    public Vector add(Vector v) {
+        // Implement me!
+    }
+
+    public Vector scale(double magnitude) {
+        // Implement me!
+    }
+
+}
